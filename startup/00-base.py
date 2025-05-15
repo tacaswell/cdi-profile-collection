df1dbf38a7a2e5aa6d5d08dcf7e4d6238064272b
jennmald <jmaldonad@bnl.gov>
2025-05-15 15:53:40 -0400

    update configure base with redis url

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,3 +1,6 @@
 import nslsii
 
-nslsii.configure_base(get_ipython().user_ns, "temp")
+nslsii.configure_base(get_ipython().user_ns, 
+                      broker_name="temp", 
+                      publish_documents_with_kafka=True, 
+                      redis_url = "info.cdi.nsls2.bnl.gov",)
