57afd02b5a460b85fa371b2d39301461f2e44c4d
jennmald <jmaldonad@bnl.gov>
2025-05-15 15:55:42 -0400

    happy linter

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,6 +1,8 @@
 import nslsii
 
-nslsii.configure_base(get_ipython().user_ns, 
-                      broker_name="temp", 
-                      publish_documents_with_kafka=True, 
-                      redis_url = "info.cdi.nsls2.bnl.gov",)
+nslsii.configure_base(
+    get_ipython().user_ns,
+    broker_name="temp",
+    publish_documents_with_kafka=True,
+    redis_url="info.cdi.nsls2.bnl.gov",
+)
