cedeb9fc4be03567229c924e4c51c7359b474bcf
Thomas Hopkins <thopkins1@bnl.gov>
2025-09-03 09:08:59 -0400

    Updated motors to use new devices and PVs (#10)
    * Updated motors to use new devices and PVs
    
    * Updated drop-in.py
    
    * Updated CI to not duplicate tests
    
    * Wait for connection in drop-in.py

--- .ci/drop-in.py
+++ .ci/drop-in.py
@@ -1,29 +1,18 @@
 motors = [
-    slt_wb1,
-    fltr_dm1,
-    fsvpm,
-    fshpm,
-    fsdm2,
-    # mono_dmm,
-    mono_hdcm,
-    slt_vpm,
-    slt_hpm,
-    mir_vpm,
-    imdm2,
-    mir_hpm,
-    slt_dm3,
-    bpm_dm3,
-    fsdm3,
-    # mir_kbv,
-    # mir_kbh,
-    # bpm_dm4,
-    # wnd_exit,
-    # gon1,
-    # slt_bcuu,
-    # slt_bcud,
-    # qstar1,
+    dm1,
+    vpm,
+    hpm,
+    dm2,
+    dmm,
+    dcm,
+    dm3,
+    kb,
+    dm4,
+    gon,
+    bcu,
 ]
 from pprint import pformat
 
 for motor in motors:
+    motor.wait_for_connection()
     print(f"{motor.name}:\n{pformat(motor.read())}")
--- startup/20-motors.py
+++ startup/20-motors.py
@@ -1,96 +1,25 @@
 from cditools.motors import (
-    BPMDM3,
-    BPMDM4,
-    FSDM2,
-    FSDM3,
-    FSHPM,
-    FSVPM,
-    IMDM2,
-    FltrDM1,
-    Gon1,
-    MirHPM,
-    MirKBh,
-    MirKBv,
-    MirVPM,
-    MonoDMM,
-    MonoHDCM,
-    Qstar1,
-    SltBCUD,
-    SltBCUU,
-    SltDM3,
-    SltHPM,
-    SltVPM,
-    SltWB1,
-    WndExit,
+    BCU,
+    DCM,
+    DM1,
+    DM2,
+    DM3,
+    DM4,
+    DMM,
+    GON,
+    HPM,
+    KB,
+    VPM,
 )
 
-slt_wb1 = SltWB1(prefix="XF:09IDA-OP:1{Slt:WB1", name="slt_wb1", labels=["motors"])
-slt_wb1.wait_for_connection(timeout=10.0)
-
-fltr_dm1 = FltrDM1(prefix="XF:09IDA-OP:1{Fltr:DM1", name="fltr_dm1", labels=["motors"])
-fltr_dm1.wait_for_connection(timeout=10.0)
-
-fsvpm = FSVPM(prefix="XF:09IDA-OP:1{FS:VPM", name="fsvpm", labels=["motors"])
-fsvpm.wait_for_connection(timeout=10.0)
-
-fshpm = FSHPM(prefix="XF:09IDA-OP:1{FS:HPM", name="fshpm", labels=["motors"])
-fshpm.wait_for_connection(timeout=10.0)
-
-fsdm2 = FSDM2(prefix="XF:09IDA-OP:1{FS:DM2", name="fsdm2", labels=["motors"])
-fsdm2.wait_for_connection(timeout=10.0)
-
-# mono_dmm = MonoDMM(prefix="XF:09IDA-OP:1{Mono:DMM", name="mono_dmm", labels=["motors"])
-# mono_dmm.wait_for_connection(timeout=10.0)
-
-mono_hdcm = MonoHDCM(
-    prefix="XF:09IDA-OP:1{Mono:HDCM", name="mono_hdcm", labels=["motors"]
-)
-mono_hdcm.wait_for_connection(timeout=10.0)
-
-slt_vpm = SltVPM(prefix="XF:09IDA-OP:1{Slt:VPM", name="slt_vpm", labels=["motors"])
-slt_vpm.wait_for_connection(timeout=10.0)
-
-slt_hpm = SltHPM(prefix="XF:09IDA-OP:1{Slt:HPM", name="slt_hpm", labels=["motors"])
-slt_hpm.wait_for_connection(timeout=10.0)
-
-mir_vpm = MirVPM(prefix="XF:09IDA-OP:1{Mir:VPM", name="mir_vpm", labels=["motors"])
-mir_vpm.wait_for_connection(timeout=10.0)
-
-imdm2 = IMDM2(prefix="XF:09IDA-OP:1{IM:DM2", name="imdm2", labels=["motors"])
-imdm2.wait_for_connection(timeout=10.0)
-
-mir_hpm = MirHPM(prefix="XF:09IDA-OP:1{Mir:HPM", name="mir_hpm", labels=["motors"])
-mir_hpm.wait_for_connection(timeout=10.0)
-
-slt_dm3 = SltDM3(prefix="XF:09IDB-OP:1{Slt:DM3", name="slt_dm3", labels=["motors"])
-slt_dm3.wait_for_connection(timeout=10.0)
-
-bpm_dm3 = BPMDM3(prefix="XF:09IDB-OP:1{BPM:DM3", name="bpm_dm3", labels=["motors"])
-bpm_dm3.wait_for_connection(timeout=10.0)
-
-fsdm3 = FSDM3(prefix="XF:09IDB-OP:1{FS:DM3", name="fsdm3", labels=["motors"])
-fsdm3.wait_for_connection(timeout=10.0)
-
-# mir_kbv = MirKBv(prefix="XF:09IDC-OP:1{Mir:KBv", name="mir_kbv", labels=["motors"])
-# mir_kbv.wait_for_connection(timeout=10.0)
-#
-# mir_kbh = MirKBh(prefix="XF:09IDC-OP:1{Mir:KBh", name="mir_kbh", labels=["motors"])
-# mir_kbh.wait_for_connection(timeout=10.0)
-#
-# bpm_dm4 = BPMDM4(prefix="XF:09IDC-OP:1{BPM:DM4", name="bpm_dm4", labels=["motors"])
-# bpm_dm4.wait_for_connection(timeout=10.0)
-#
-# wnd_exit = WndExit(prefix="XF:09IDC-OP:1{Wnd:Exit", name="wnd_exit", labels=["motors"])
-# wnd_exit.wait_for_connection(timeout=10.0)
-#
-# gon1 = Gon1(prefix="XF:09IDC-OP:1{Gon:1", name="gon1", labels=["motors"])
-# gon1.wait_for_connection(timeout=10.0)
-#
-# slt_bcuu = SltBCUU(prefix="XF:09IDC-OP:1{Slt:BCUU", name="slt_bcuu", labels=["motors"])
-# slt_bcuu.wait_for_connection(timeout=10.0)
-#
-# slt_bcud = SltBCUD(prefix="XF:09IDC-OP:1{Slt:BCUD", name="slt_bcud", labels=["motors"])
-# slt_bcud.wait_for_connection(timeout=10.0)
-#
-# qstar1 = Qstar1(prefix="XF:09IDC-OP:1{Qstar:1", name="qstar1", labels=["motors"])
-# qstar1.wait_for_connection(timeout=10.0)
+dm1 = DM1(prefix="XF:09IDA-OP:1{", name="dm1", labels=["motors"])
+vpm = VPM(prefix="XF:09IDA-OP:1{", name="vpm", labels=["motors"])
+hpm = HPM(prefix="XF:09IDA-OP:1{", name="hpm", labels=["motors"])
+dm2 = DM2(prefix="XF:09IDA-OP:1{", name="dm2", labels=["motors"])
+dmm = DMM(prefix="XF:09IDA-OP:1{", name="dmm", labels=["motors"])
+dcm = DCM(prefix="XF:09IDA-OP:1{", name="dcm", labels=["motors"])
+dm3 = DM3(prefix="XF:09IDB-OP:1{", name="dm3", labels=["motors"])
+kb = KB(prefix="XF:09IDC-OP:1{", name="kb", labels=["motors"])
+dm4 = DM4(prefix="XF:09IDC-OP:1{", name="dm4", labels=["motors"])
+gon = GON(prefix="XF:09IDC-OP:1{", name="gon", labels=["motors"])
+bcu = BCU(prefix="XF:09IDC-OP:1{", name="bcu", labels=["motors"])
