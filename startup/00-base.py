0e5e99d5cdb9cc819cadf8099584c33f0b4d0291
Max Rakitin <mrakitin@bnl.gov>
2025-09-05 16:23:37 -0400

    Do not publish docs to kafka

--- startup/00-base.py
+++ startup/00-base.py
@@ -3,6 +3,6 @@ import nslsii
 nslsii.configure_base(
     get_ipython().user_ns,
     broker_name="temp",
-    publish_documents_with_kafka=True,
+    publish_documents_with_kafka=False,
     redis_url="info.cdi.nsls2.bnl.gov",
 )
