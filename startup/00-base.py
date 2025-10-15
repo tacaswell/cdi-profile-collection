6626a22e5a8614186997617d1bf19be24bb0448a
xf09id1 <gbischof@bnl.gov>
2025-10-15 16:07:41 -0400

    add TiledWriter

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,8 +1,28 @@
 import nslsii
+import os
+
+from tiled.client import from_profile, from_uri
+from bluesky.callbacks.tiled_writer import TiledWriter
+from ophyd.sim import det1, det2
+from bluesky.plans import count
 
 nslsii.configure_base(
     get_ipython().user_ns,
-    broker_name="temp",
     publish_documents_with_kafka=False,
-    redis_url="info.cdi.nsls2.bnl.gov",
 )
+
+tiled_writing_client = from_uri(
+    "https://tiled-staging.nsls2.bnl.gov/api/v1/metadata/cdi/raw",
+    api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_CDI"],
+)
+
+c = tiled_reading_client = from_uri(
+    "https://tiled-staging.nsls2.bnl.gov/api/v1/metadata/cdi/raw",
+    include_data_sources=True,
+)
+
+RE.md["tiled_access_tags"] = ["cdi_beamline"]
+
+tw = TiledWriter(client=tiled_writing_client)
+
+RE.subscribe(tw)
